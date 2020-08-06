5e30e0d70e5f46ae987cefb60379831fd34dd67b
Samir Bennafla <samir.bennafla@gmail.com>
2020-08-06 15:55:02 +0900

    Mister cocktail need to create the doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:index]
+  end
+  resources :doses, only: [:destroy]
 end
